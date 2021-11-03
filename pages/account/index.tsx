a0cfa7af041604b80fcd4224d0800232b23db193
= <rhyswoolcott@gmail.com>
2021-11-03 18:16:27 +0000

    idk what i added

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -5,13 +5,21 @@ import { useEffect, useState } from "react";
 import { basePath } from "../../utils/config";
 import { supabase } from "../../utils/supabaseClient";
 
+export async function getServerSideProps() {
+  let cookie = Cookies.get("auth");
+  if (cookie) console.log("Cookie Found");
+
+  return { props: { cookie } };
+}
+
 export default function Account({ cookie }: any) {
   let [session, setSession] = useState<Session | null>(null);
   let router = useRouter();
+  let Cookie = Cookies.get("auth");
 
   return (
     <>
-      <button>Click Me</button>
+      <h1>{Cookie}</h1>
     </>
   );
 }
